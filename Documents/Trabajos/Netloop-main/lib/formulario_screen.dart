a3c7f7442e8873972472a4b60537b2bca141f043
1013099676-commits <1013099676@teinco.edu.co>
2025-09-30 04:06:25 -0500

    Update formulario_screen.dart

--- Documents/Trabajos/Netloop-main/lib/formulario_screen.dart
+++ Documents/Trabajos/Netloop-main/lib/formulario_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FormularioScreen extends StatefulWidget {
